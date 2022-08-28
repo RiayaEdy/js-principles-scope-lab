23c00753ef5453f9ad8cb9a352078b179761f52a
RiayaEdy <edywidowaty@gmail.com>
2022-08-28 23:01:43 +1000

    Understandhow functions determine scope in JS

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+function customerName() {
+    return customerName();
+}
+customerName = 'bob'
+
+function upperCaseCustomerName () {
+    window.customerName = (window.customerName || '').toUpperCase();
+}
+
+
+function setBestCustomer () {
+    window.bestCustomer = (window.bestCustomer || 'not bob').toString( 'not bob');
+}
+
+function overwriteBestCustomer () {
+   window.bestCustomer = ( 'maybe bob');
+}
+
+const leastFavoriteCustomer = 'nik';
+function changeLeastFavoriteCustomer () {
+    throw'Assignment to constant variable.';
+}
